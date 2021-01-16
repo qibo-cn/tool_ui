0f3f8bd364bc69867ca042c6f0b7c6eeb66dcedb
lichang98 <15365811995@163.com>
2021-01-16 14:05:08 +0800

    fix tab toggle problem

--- inner_scripts/model_desc.py
+++ inner_scripts/model_desc.py
@@ -7,9 +7,21 @@ import keras
 import sys
 import json
 import numpy as np
-from tensorflow.python.eager.function import _POSSIBLE_GRADIENT_TYPES_FIRST_ORDER
+from PIL import Image
+import os
 
-model_file = sys.argv[1]
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+x_norm_file = sys.argv[1]
+x_test_file = sys.argv[2]
+y_test_file = sys.argv[3]
+
+
+x_norm = np.load(x_norm_file)["arr_0"]
+x_test = np.load(x_test_file)["arr_0"]
+y_test = np.load(y_test_file)["arr_0"]
+
+model_file = sys.argv[4]
 print("py script get model file path [{}]".format(model_file))
 model = keras.models.load_model(model_file)
 
@@ -38,3 +50,32 @@ with open("model_general_info.json","w+",encoding="utf-8") as f:
 # save info of each layer
 with open("model_layers_info.json","w+",encoding="utf-8") as f:
     f.write(json.dumps(model_layers_info))
+
+
+# visualize the conv and activation layers
+layer_vis_data=[]
+idx=0
+max_vis_each_layer=5
+
+for layer in model.layers:
+    idx +=1
+    if layer.__class__.__name__ == "Conv2D" or layer.__class__.__name__ == "Activation":
+        layer_output = keras.models.Model(inputs=model.input, outputs=layer.output).predict(x_norm[0:1])
+        layer_output = layer_output[0]
+        print("shape of layer output={}".format(np.shape(layer_output)))
+        layer_vis_img_paths=[]
+        for i in range(min(np.shape(layer_output)[-1], max_vis_each_layer)):
+            img = np.array(layer_output[:,:,i],dtype="float32")*255.0
+            img = np.array(img, dtype="uint8")
+            img = Image.fromarray(img)
+            img = img.resize((32,32))
+            img.save(os.path.join(base_path,"layer_vis_imgs", "layer_{}_{}_vis.png".format(idx,i)))
+            layer_vis_img_paths.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), \
+                "layer_vis_imgs", "layer_{}_{}_vis.png".format(idx,i)))
+        layer_vis_data.append({"layer_name": layer.__class__.__name__, "layer_index":idx, \
+            "layer_vis_img_paths": layer_vis_img_paths})
+
+# save layer images info
+with open("layer_vis_info.json","w+") as f:
+    f.write(json.dumps(layer_vis_data))
+
